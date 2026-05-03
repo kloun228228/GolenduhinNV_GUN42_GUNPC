e8604de9c980def1542fd3995e489234496811b4
kloun228228 <lassoktop@gmail.com>
2026-05-03 15:29:56 +0500

    Add files via upload

---
+++ Assets/Scripts/EditorMover.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+	
+	[RequireComponent(typeof(PositionSaver))]
+	public class EditorMover : MonoBehaviour
+	{
+		private PositionSaver _save;
+		private float _currentDelay;
+
+        //todo comment: Что произойдёт, если _delay > _duration?
+        //Запись завершится раньше, чем сработает первый интервал таймера.
+        [SerializeField, Range(0.2f, 1.0f)]
+		private float _delay = 0.5f;
+		[SerializeField, Min(0.2f)]
+		private float _duration = 5f;
+
+		private void Start()
+		{
+           
+                //todo comment: Почему этот поиск производится здесь, а не в начале метода Update?
+                //В Start она выполняется один раз, а в Update она бы выполнялась 60–100 раз в секунду, что сильно ударило бы по производительности
+                _save = GetComponent<PositionSaver>();
+            if (_duration < _delay * 5)
+			{
+
+			}
+                _save.Records.Clear();
+		}
+
+		private void Update()
+		{
+			_duration -= Time.deltaTime;
+			if (_duration <= 0f)
+			{
+				enabled = false;
+				Debug.Log($"<b>{name}</b> finished", this);
+				return;
+			}
+
+            //todo comment: Почему не написать (_delay -= Time.deltaTime;) по аналогии с полем _duration?
+            //Если мы будем вычитать из неё время, каждый следующий интервал записи будет становиться короче предыдущего. Для отсчета нам нужна отдельная переменная-таймер (
+            _currentDelay -= Time.deltaTime;
+			if (_currentDelay <= 0f)
+			{
+				_currentDelay = _delay;
+				_save.Records.Add(new PositionSaver.Data
+				{
+					Position = transform.position,
+                    //todo comment: Для чего сохраняется значение игрового времени?
+                    //Чтобы в ReplayMover мы могли воспроизвести движение не просто по очереди, а с той же скоростью и таймингами, с которыми оно записывалось.
+                    Time = Time.time,
+				});
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ReplayMover.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+	[RequireComponent(typeof(PositionSaver))]
+	public class ReplayMover : MonoBehaviour
+	{
+		private PositionSaver _save;
+
+		private int _index;
+		private PositionSaver.Data _prev;
+		private float _duration;
+
+		private void Start()
+		{
+            ////todo comment: зачем нужны эти проверки?
+            //// Чтобы убедиться, что компонент PositionSaver найден и в нём есть записанные точки. Без этого код выдаст ошибку.
+            if (!TryGetComponent(out _save) || _save.Records.Count == 0)
+			{
+				Debug.LogError("Records incorrect value", this);
+                //todo comment: Для чего выключается этот компонент?
+                //Чтобы остановить выполнение метода Update и сэкономить ресурсы, если данных для воспроизведения нет.
+                enabled = false;
+			}
+		}
+
+		private void Update()
+		{
+			var curr = _save.Records[_index];
+			//todo comment: Что проверяет это условие (с какой целью)? 
+			if (Time.time > curr.Time)
+			{
+				_prev = curr;
+				_index++;
+                //todo comment: Для чего нужна эта проверка?
+                //Чтобы избежать ошибки при делении на ноль, если время между двумя точками равно нулю.
+                if (_index >= _save.Records.Count)
+				{
+					enabled = false;
+					Debug.Log($"<b>{name}</b> finished", this);
+#if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+#endif
+                }
+			}
+            //todo comment: Для чего производятся эти вычисления (как в дальнейшем они применяются)?
+            //Вычисляется процент прогресса времени между двумя точками (от 0 до 1), чтобы понять, в какой части пути сейчас должен находиться объект.
+            var delta = (Time.time - _prev.Time) / (curr.Time - _prev.Time);
+			//todo comment: Зачем нужна эта проверка?
+			if (float.IsNaN(delta)) delta = 0f;
+            //todo comment: Опишите, что происходит в этой строчке так подробно, насколько это возможно
+            //Функция плавно перемещает объект из точки _prev в точку curr на расстояние, равное коэффициенту delta.
+            transform.position = Vector3.Lerp(_prev.Position, curr.Position, delta);
+		}
+	}
+}
